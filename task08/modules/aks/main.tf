2d5e8c3b1f4093374c07cb5187700d6f2e1914e3
ioanndubinets-devops <ioann_dubinets@epam.com>
2025-10-19 22:09:19 +0300

    Update main.tf

--- task08/modules/aks/main.tf
+++ task08/modules/aks/main.tf
@@ -30,12 +30,13 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
 
 resource "azurerm_key_vault_access_policy" "aks_kv_access" {
   key_vault_id = var.key_vault_id
-  tenant_id    = azurerm_kubernetes_cluster.aks.identity[0].tenant_id
-  object_id    = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  tenant_id    = azurerm_kubernetes_cluster.aks.key_vault_secrets_provider[0].secret_identity[0].tenant_id
+  object_id    = azurerm_kubernetes_cluster.aks.key_vault_secrets_provider[0].secret_identity[0].object_id
 
   secret_permissions = [
     "Get",
     "List"
   ]
 }
+
 
