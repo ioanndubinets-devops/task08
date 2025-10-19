ed7e1caa407f622bc2bdb5f50c97ebc3bff32841
ioanndubinets-devops <ioann_dubinets@epam.com>
2025-10-19 23:03:15 +0300

    Update main.tf

--- task08/modules/aks/main.tf
+++ task08/modules/aks/main.tf
@@ -1,3 +1,10 @@
+resource "azurerm_user_assigned_identity" "aks_csi_identity" {
+  name                = "${var.aks_name}-csi-identity" # Даємо унікальне ім'я
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_name
   location            = var.location
@@ -16,7 +23,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   key_vault_secrets_provider {
-    secret_rotation_enabled = true
+    secret_rotation_enabled   = true
+    secret_identity_object_id = azurerm_user_assigned_identity.aks_csi_identity.principal_id
   }
 
   tags = var.tags
@@ -30,14 +38,13 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
 
 resource "azurerm_key_vault_access_policy" "aks_kv_access" {
   key_vault_id = var.key_vault_id
-  tenant_id    = azurerm_kubernetes_cluster.aks.identity[0].tenant_id
-  object_id    = azurerm_kubernetes_cluster.aks.key_vault_secrets_provider[0].secret_identity[0].object_id
+  # tenant_id    = azurerm_kubernetes_cluster.aks.identity[0].tenant_id
+  # object_id    = azurerm_kubernetes_cluster.aks.key_vault_secrets_provider[0].secret_identity[0].object_id
+  tenant_id = azurerm_user_assigned_identity.aks_csi_identity.tenant_id
+  object_id = azurerm_user_assigned_identity.aks_csi_identity.principal_id
 
   secret_permissions = [
     "Get",
     "List"
   ]
 }
-
-
-
